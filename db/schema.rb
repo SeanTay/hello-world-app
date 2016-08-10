4339d9e9a876dab5e0e4343c241f8aa5abafba48
Sean Tay <seantay@Seans-MacBook-Pro-2.local>
2016-08-10 18:52:33 -0400

    nothing changed

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160810142544) do
+ActiveRecord::Schema.define(version: 20160810183058) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +30,14 @@ ActiveRecord::Schema.define(version: 20160810142544) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "todos", force: :cascade do |t|
+    t.text     "body"
+    t.string   "tag"
+    t.string   "duedate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -46,14 +53,6 @@ ActiveRecord::Schema.define(version: 20160810142544) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
-  create_table "todos", force: :cascade do |t|
-    t.text     "body"
-    t.string   "tag"
-    t.string   "duedate"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-
   end
 
   create_table "welcomes", force: :cascade do |t|
