694786aa80f4abdd26003f8b8ce6803ca7589ce1
Sean Tay <seantay@Seans-MacBook-Pro-2.local>
2016-08-11 16:43:08 -0400

    julia please don't work on master

--- app/assets/javascripts/components/Dashboard.jsx
+++ app/assets/javascripts/components/Dashboard.jsx
@@ -1,119 +1,112 @@
-
 class Dashboard extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      savedJobs: [],
-      savedTodos: [],
-      loading: false
-    }
-  }
+ constructor(props) {
+   super(props)
+   this.state = {
+     savedJobs: [],
+     savedTodos: [],
+     savedPosts: []
 
-  componentDidMount() {
-    console.log('jons mount')
-    $.getJSON('/api/jobs.json',
-    (response) => { this.setState({
-      savedJobs: response
-      })
-    })
-      console.log('todos mount')
-      $.getJSON('/api/todos.json',
-      (response) => {this.setState({
-        savedTodos: response,
-        loading: true
-      })
-    })
+   }
+ }
+
+ componentDidMount() {
+   console.log('componentdidmount')
+   $.getJSON('/api/jobs.json',
+   (response) => { this.setState({
+     savedJobs: response
+   })
+ })
+ console.log('todos mount')
+ $.getJSON('/api/todos.json',
+ (response) => {this.setState({
+   savedTodos: response
+ })
+})
 }
 
 handleDelete(e, id) {
-  let component = this
-  e.preventDefault()
-  console.log('delete item clicked')
-  $.ajax({
-    url: '/api/jobs/'+ id,
-    type: 'DELETE',
-    success: () => {
-      newJobs = component.state.savedJobs.filter((job) => {
-        return job.id != id;
-      });
-      component.setState({ savedJobs: newJobs});
-    }
-  });
+ let component = this
+ e.preventDefault()
+ console.log('delete item clicked')
+ $.ajax({
+   url: '/api/jobs/'+ id,
+   type: 'DELETE',
+   success: () => {
+     newJobs = component.state.savedJobs.filter((job) => {
+       return job.id != id;
+     });
+     component.setState({ savedJobs: newJobs});
+   }
+ });
 }
 
+
 handleSubmitTodo (e, item) {
-  console.log("in the handle submit to do function")
-  //make an AJAX CALL
-  $.getJSON('/api/todos.json',
-  (response) => {this.setState({
-    savedTodos: response,
-    loading: true
-  })
+ console.log("in the handle submit to do function")
+ $.getJSON('/api/todos.json',
+ (response) => {this.setState({
+   savedTodos: response
+ })
 })
-  console.log('newstate',this.state)
-
-  // let savedTodos = this.state.savedTodos
-  // savedTodos.push(item)
-  // this.setState ({
-  //   savedTodos
-  // })
-
 }
 
 handleDeleteTodo(e, id){
-  let component = this
-  e.preventDefault()
-  console.log('delete todo item clicked')
-  $.ajax({
-    url: '/api/todos/' + id,
-    type: 'DELETE',
-    success: () => {
+ let component = this
+ e.preventDefault()
+ console.log('delete todo item clicked')
+ $.ajax({
+   url: '/api/todos/' + id,
+   type: 'DELETE',
+   success: () => {
 
-      console.log('successfully deleting backend')
-      newTodos = component.state.savedTodos.filter((todo) => {
-        return todo.id != id;
-      });
-      component.setState({savedTodos: newTodos})
-    },
-    fail: () => {
-      console.log('failed')
-    }
-  })
+     console.log('successfully deleting backend')
+     newTodos = component.state.savedTodos.filter((todo) => {
+       return todo.id != id;
+     });
+     component.setState({savedTodos: newTodos})
+   }
+ })
 }
 
 handleSubmit(e, job) {
-  var newState = this.state.savedJobs.concat(e, job);
-  this.setState({
-    savedJobs: newState,
-  })
+ console.log('handle submit')
+ var newState = this.state.savedJobs.concat(e, job);
+ this.setState({
+   savedJobs: newState,
+ })
+}
+
+handlePostSubmit(e, job) {
+ console.log('handle submit')
+ var newState = this.state.savedPosts.concat(e, post);
+ this.setState({
+   savedJobs: newState,
+ })
 }
 
 render () {
-  return (
+ console.log("passing state from dashboard",this.state.savedTodos)
+ return (
 
-    <div className = "dashboard">
-      <div className = "filler">
-        <p> left box</p>
-      </div>
-        <List
-          todos = {this.state.savedTodos}
-          handleDeleteTodo={(e, id) => this.handleDeleteTodo(e, id)}
-          handleSubmitTodo = {(e, item)=>this.handleSubmitTodo(e,item)}
-          />
+   <div className = "dashboard">
+     <div className = "filler">
+       <BlogContainer
+         handlePostSubmit = {(e,post) =>this.handlePostSubmit(e,post)}/>
+     </div>
 
+     <List
+       todos = {this.state.savedTodos}
+       handleDeleteTodo={(e, id) => this.handleDeleteTodo(e, id)}
+       handleSubmitTodo = {(e, item)=>this.handleSubmitTodo(e,item)}
+       />
 
+     <SavedJobs
+       savedJobs = {this.state.savedJobs}
+       handleDelete ={(e, id) => this.handleDelete(e, id)}
+       />
+   </div>
 
-        <SavedJobs
-          savedJobs = {this.state.savedJobs}
-          handleDelete ={(e, id) => this.handleDelete(e, id)}
-          />
-
-          <NewItem
-            handleSubmitTodo={(e,item)=>this.handleSubmitTodo(e,item)}/>
-
-    </div>
-
-  )
+ )
 
 }
 }
